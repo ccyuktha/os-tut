01854e1e0d49c9f0ed1f24e03da9d37886cb8736
KHCharan13 <baasu.kondeti@gmail.com>
2021-12-30 23:00:39 +0530

    Update sjf.c
    Co-Authored-By: ccyuktha <95213530+ccyuktha@users.noreply.github.com>

--- sjf.c
+++ sjf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main() 
 {
-      int arrival_time[10], burst_time[10], temp[10],bt[10];
-      int i, smallest, count = 0, time, limit;
+      int at[10], bt[10], temp[10],bt[10];
+      int i, lowest, count = 0, time, limit;
       double wait_time = 0, turnaround_time = 0, end,ct[10];
-      float average_waiting_time, average_turnaround_time;
+      float avg_wt, avg_tat;
       float wt[10],tat[10];
       printf("Enter the Total Number of Processes:\t");
       scanf("%d", &limit); 
@@ -13,45 +13,45 @@ int main()
       {
             printf("\nDetails of process %d \n",i+1);
             printf("Enter Arrival Time:\t");
-            scanf("%d", &arrival_time[i]);
+            scanf("%d", &at[i]);
             printf("Enter Burst Time:\t");
-            scanf("%d", &burst_time[i]); 
-            temp[i] = burst_time[i];
-            bt[i]= burst_time[i];
+            scanf("%d", &bt[i]); 
+            temp[i] = bt[i];
+            bt[i]= bt[i];
       }
-      burst_time[9] = 9999;  
+      bt[9] = 9999;  
       for(time = 0; count != limit; time++)
       {
-            smallest = 9;
+            lowest = 9;
             for(i = 0; i < limit; i++)
             {
-                  if(arrival_time[i] <= time && burst_time[i] < burst_time[smallest] && burst_time[i] > 0)
+                  if(at[i] <= time && bt[i] < bt[lowest] && bt[i] > 0)
                   {
-                        smallest = i;
+                        lowest = i;
                   }
             }
-            burst_time[smallest]--;
-            if(burst_time[smallest] == 0)
+            bt[lowest]--;
+            if(bt[lowest] == 0)
             {
                   count++;
                   end = time + 1;
-                  ct[smallest]=end;
-                  wait_time = wait_time + end - arrival_time[smallest] - temp[smallest];
+                  ct[lowest]=end;
+                  wait_time = wait_time + end - at[lowest] - temp[lowest];
                  
-                  turnaround_time = turnaround_time + end - arrival_time[smallest];
+                  turnaround_time = turnaround_time + end - at[lowest];
                  
             }
       }
-      average_waiting_time = wait_time / limit; 
-      average_turnaround_time = turnaround_time / limit;
+      avg_wt = wait_time / limit; 
+      avg_tat = turnaround_time / limit;
       printf("p\tat\tbt\tct      \twt      \ttat\n");
       for(i = 0; i < limit; i++){
-          tat[i]=ct[i]-arrival_time[i];
+          tat[i]=ct[i]-at[i];
           wt[i]=tat[i]-bt[i];
-          printf("%d\t%d\t%d\t%f\t%f\t%f\n",i+1,arrival_time[i],bt[i],ct[i],wt[i],tat[i]);
+          printf("%d\t%d\t%d\t%f\t%f\t%f\n",i+1,at[i],bt[i],ct[i],wt[i],tat[i]);
       }
       
-      printf("\nAverage Waiting Time:\t %lf \n", average_waiting_time);
-      printf("Average Turnaround Time:\t %lf \n", average_turnaround_time);
+      printf("\navg wt Time:\t %lf \n", avg_wt);
+      printf("avg Turnaround Time:\t %lf \n", avg_tat);
       return 0;
 }
